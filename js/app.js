e6a80c990c934fecc890ef4a6e0e6c784c446f0e
YesicaGV9 <yesicagonzalezv99@gmail.com>
2026-04-06 17:11:04 -0500

    arreglo letras

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-const cards = document.querySelectorAll(".card");
-const btnVocales = document.getElementById("btn-vocales");
-const btnTodas = document.getElementById("btn-todas");
+let contador = 0;
 
-btnVocales.addEventListener("click", () => {
-  cards.forEach(card => {
-    card.style.display =
-      card.dataset.tipo === "vocal" ? "block" : "none";
-  });
-});
+function voltear(card) {
+  const yaVista = card.classList.contains("volteada");
 
-btnTodas.addEventListener("click", () => {
+  // Agrega o quita la clase .volteada
+  card.classList.toggle("volteada");
+
+  // Solo suma al contador si es la PRIMERA vez que se voltea
+  if (!yaVista) {
+    contador++;
+    document.getElementById("contador").textContent = contador;
+  }
+}
+
+function filtrar(tipo) {
+  const cards = document.querySelectorAll(".card");
+
   cards.forEach(card => {
-    card.style.display = "block";
+    if (tipo === "todas") {
+      card.style.display = "block";
+    } else if (tipo === "vocales") {
+      card.style.display =
+        card.dataset.tipo === "vocal" ? "block" : "none";
+    }
   });
-});
+}
